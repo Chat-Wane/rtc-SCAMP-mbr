b9679e8d48842ceccb4dfb4ca439e277916395fd
chat-wane <grumpy.chat.wane@gmail.com>
2014-11-21 16:55:27 +0100

    connection from p1 to p2 works, p2 to p1 automatically made, however, connections forwarded to network not working yet

--- scampjs.bundle.js
+++ scampjs.bundle.js
@@ -1,15 +1,14 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports.c = 0;
-module.exports.partialViewSize = 10;
-module.exports.inViewSize = 10; 
+
 module.exports.heartbeat = 30000; 
 
 },{}],2:[function(require,module,exports){
 /*!
  * MSubscriptionRequest(uid,counter,offer)
  * MSubscriptionResponse(uid,counter,destUid,destCounter,offer)
- * MOfferRequest(uid,destUid,k)
- * MOfferResponse(uid,destUid,offers)
+ * MOfferRequest(uid,counter,k)
+ * MOfferResponse(uid,destUid,destCounter,offers)
  * MHeartbeat(uid)
  */
 
@@ -84,6 +83,7 @@ function MOfferResponse(uid, destUid, destCounter, offers){
     this.category = "response";
     this.uid = uid;
     this.destUid = destUid;
+    this.destCounter = destCounter;
     this.offers = offers;
 };
 
@@ -7916,13 +7916,13 @@ Peer.prototype.generateOffers = function(k){
     for (var i = 0; i<k; ++i){
 	var pair = this.track.increment();
 	var link = new SimplePeer({initiator:true});
-	link.index = this.sub.length;
-	this.sub.push(new MSubscriptionRequest(pair._e,
-					       pair._c,
-					       []));
-	this.pending.push(link);
+	link.index = pair._c;
+	this.sub[pair._c] = new MSubscriptionRequest(pair._e,
+						     pair._c,
+						     []);
+	this.pending[pair._c] = link;
 	this.setLink(link);
-	offers.push(this.sub[i]);
+	offers.push(this.sub[pair._c]);
     };
     
     setTimeout(function(){
@@ -7949,13 +7949,13 @@ Peer.prototype.onContact = function(uid, counter, offer){
     // #1 create the peer and signal it the incomming offer
     var firstLink = new SimplePeer();
     for (var i = 0; i<offer.length; ++i){firstLink.signal(offer[i]);};
-    firstLink.index = this.sub.length;
     // #2 initialize the accept message and put the link in pending
     var pair = self.track.increment();
-    this.sub.push(new MSubscriptionResponse(pair._e, pair._c,
-					    uid, counter,
-					    []));
-    this.pending.push(firstLink);
+    firstLink.index = pair._c;
+    this.sub[pair._c] = new MSubscriptionResponse(pair._e, pair._c,
+						  uid, counter,
+						  []);
+    this.pending[pair._c] = firstLink;
     this.setLink(firstLink);
     // #3 preparing the link to send some offer into the network
     firstLink.on("message", function(message){
@@ -7988,19 +7988,18 @@ Peer.prototype.onContact = function(uid, counter, offer){
  */
 Peer.prototype.onContactAccepted = function(uid, counter, offer){
     var self = this;
-    var position = counter-1;
-    this.pending[position].on("ready", function(){
-	console.log("Send me " + self.partialView.length+1+" offers, friend.");
-	self.pending[position].send(
-	    new MOfferRequest(self.track.local.e,
-			      uid,
-			      self.partialView.length + 1 + Conf.c));
+    this.pending[counter].on("ready", function(){
+	console.log("Send me "+(self.partialView.length+1)+" offers, friend.");
+	var pair = self.track.increment();
+	self.pending[counter].send(
+	    new MOfferRequest(pair._e, pair._c,
+			      self.partialView.length + Conf.c + 1));
 	var viewEntry = new ViewEntry(uid, this);
 	var pos = self.inView.binaryIndexOf(uid);
 	self.inView.splice(pos,0,viewEntry);
     });
     for (var i = 0; i<offer.length; ++i){
-	this.pending[position].signal(offer[i]);	
+	this.pending[counter].signal(offer[i]);	
     };
 };
 
@@ -8011,32 +8010,72 @@ Peer.prototype.onContactAccepted = function(uid, counter, offer){
 Peer.prototype.setLink = function(link){
     var self = this;
     link.on('signal', function(data){
-	self.sub[this.index].offer.push(data);
+	if (("sdp" in data) ||
+	    (("candidate" in data)&&(data.candidate.sdpMid=="data"))){
+	    self.sub[this.index].offer.push(data);
+	};
     });
     link.on('ready', function(){
 	console.log("New friend, yaay ! :3");
     });
     link.on('message', function(message){
-	self.receive(link, message);
+	self.receive(this, message);
+    });
+    link.on('error', function(err){
+	// (TODO)
     });
 }
 
 /*!
  * \brief redirect the processing of the message to the proper function (e.g.
  * a new subscription message will be redirected to the function onSubscription
+ * \param link the WebRTC that receive the message
  * \param message the received message
  */
 Peer.prototype.receive = function(link, message){
     // #0 check if we already received this message in the past
-    // #1 check if the message is for "this" peer
+    if (message.category == "request"){
+	this.backtrack.set({_e:message.uid, _c:message.counter},link);
+    };
+    // #1 special kind of message actually containing request to fwd
     if (message.type == "MOfferResponse"){
 	console.log("Just received "+message.offers.length+" offers!!");
-	self.onSubscription(message.uid, message.offers); return;
+	for (var i = 0; i<message.offers.length; ++i){
+	    this.backtrack.set({_e:message.offers[i].uid,
+				_c:message.offers[i].counter},link);
+	};
+	this.onSubscription(message.offers); return;
     };
+    // #2 the message is a response to a particular message
+    if (message.category == "response"){
+	// #2a this message is for 'this' peer
+	if (message.destUid == this.track.local.e){	    
+	    if (message.type == "MSubscriptionResponse"){
+		var self = this;
+		var position = message.destCounter;
+		this.pending[position].on("ready", function(){
+		    console.log("Backtracted sub' works!! yayyy!");
+		    var viewEntry = new ViewEntry(message.uid, this);
+		    var pos = self.inView.binaryIndexOf(message.uid);
+		    self.inView.splice(pos,0,viewEntry);
+		});
+		for (var i = 0; i<message.offer.length; ++i){
+		    this.pending[position].signal(message.offer[i]);
+		};
+	    };
+	    return;
+	};
+	// #2b this message must be backtracked
+	var pair = {_e:message.destUid,_c:message.destCounter};
+	if (this.backtrack.has(pair)){
+	    this.backtrack.get(pair).send(message);
+	};
+	return;
+    };
+    
     if (message.type == "MHeartbeat"){
 	console.log("Peer "+message.uid+" is alive! Good to know :3"); return;
     };
-    // #2 check if the message is a response to backtrack
 };
 
 
@@ -8063,11 +8102,13 @@ Peer.prototype.broadcast = function(message){
  * \param uid the unique site identifier which wants to enter in the network
  * \param offers the informations to contact this peer directly via webRTC
  */
-Peer.prototype.onSubscription = function(uid, offers){
+Peer.prototype.onSubscription = function(offers){
     // #0 if our partialView is empty, we keep the offer for ourself
-    if ((this.partialView.length == 0) && (offers.length != 0)){
-	
-    }
+    if ((this.partialView.length == 0) && (offers.length > 0)){
+	this.onForwardedSubscription(offers[0].uid,
+				     offers[0].counter,
+				     offers[0].offer);
+    };
     // #1 forward the subscription to each peer in the partialView + c
     // additionnal copies of the subscription
     var rn = Math.floor(Math.random()*this.partialView.length);
@@ -8082,41 +8123,43 @@ Peer.prototype.onSubscription = function(uid, offers){
  * \param uid the unique site identifier of the subscriber
  * \param dataList the necessary data required to establish the p2p link
  */
-Peer.prototype.onForwardedSubscription = function(uid, dataList){
+Peer.prototype.onForwardedSubscription = function(uid, counter, offer){
     var self = this;
     // #1 check if we add the subscription to our partialView
-    if (Math.random() <  (1/(1+this.partialView.length))){
-	// #2 check if the link is not already included
-	var position = this.partialView.binaryIndexOf(uid);
-	if (position < 0 || (position == 0 && this.partialView.length==0)){
-	    // #3 create an offer to handshake
-	    var contact = new SimplePeer();
-
-	    for (var i = 0; i< dataList.length; ++i){
-		contact.signal(dataList[i]);
-	    };
-	    contact.index = this.offers.length;
-	    this.offers[this.offers.length] = [];
-
-	    contact.on("signal", function(data){
-		self.offers[this.index].push(data);
-	    });
-
-	    setTimeout(function(i){
-		var pair = self.track.increment();
-		self.broadcast(
-		    new MSubscriptionResponse(pair._e, pair._c,
-					      uid,
-					      self.offers[i]));
-	    }, 3000, contact.index);
-	    
-	    contact.on("ready", function(data){
-		// (TODO) verify if it does not already exist
-		var position = self.inView.binaryIndexOf(uid);
-		var viewEntry = new ViewEntry(uid, this);
-		self.inView.splice(position,0,viewEntry);
-	    });
+    // (TODO) verify if it is automatically fwd if already in partialview
+    if (Math.random() < (1/(1+this.partialView.length))){
+	// #2a create the new link, add it to pending
+	var link = new SimplePeer();
+	var pair = this.track.increment();
+	link.index = pair._c;
+	this.sub[pair._c] = new MSubscriptionResponse(pair._e, pair._c,
+						      uid, counter,
+						      []);
+	this.pending[pair._c] = link;
+	this.setLink(link);
+	for (var i = 0; i<offer.length; ++i){
+	    link.signal(offer[i]);
 	};
+	
+	// #2b when the link is ready, we add it to the partialView
+	link.on("ready", function(){
+	    var position = self.partialView.binaryIndexOf(uid);
+	    var viewEntry = new ViewEntry(uid, this);
+	    self.partialView.splice(position,0,viewEntry);
+	});
+	// #2c send the response message back
+	setTimeout(function(){
+	    if (self.backtrack.has({_e:uid,_c:counter})){
+		self.backtrack.get({_e:uid,_c:counter}).send(
+		    self.sub[link.index]);
+	    };
+	},3000);
+	
+    } else {
+	// #3 otherwise, forward the message to a random peer in partial view
+	var rn = Math.floor(Math.random()*this.partialView.length);
+	this.partialView[rn].link.send(
+	    new MSubscriptionRequest(uid,counter,offer));
     };
 };
 
